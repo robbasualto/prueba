8755816697a09ae3666ffdb8cfa31a509d1d81b9
Eduardo Peña Fritz <epenafritz@gmail.com>
2021-01-21 00:33:39 -0300

    index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        <title>Prueba</title>
+    </head>
+    <body class="py-4">
+        <main>
+            <div class="container">
+                <h1>Prueba</h1>
+                <p class="lead">Listado de películas en el cine.</p>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Nombre del campo...</th>
+                            <th>Etc...</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Dato</td>
+                            <td>Etc...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </main>
+  </body>
+</html>
